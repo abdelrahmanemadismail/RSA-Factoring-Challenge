da2ab548f2f488e71c20b41b3eacc624cdee493a
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2023-10-28 05:56:25 -0700

    Using long long

--- factors.c
+++ factors.c
@@ -2,51 +2,18 @@
 #include <stdlib.h>
 
 /**
- * atoll128 - Convert a string to a __int128 integer
- * @str: The string to convert
- * Return: The resulting __int128 integer
- */
-__int128 atoll128(char *str)
-{
-	__int128 result = 0;
-
-	while (*str != '\n')
-	{
-		result *= 10;
-		result += *str - '0';
-		str++;
-	}
-	return (result);
-}
-/**
- * print128 - Print a __int128 integer
- * @x: The __int128 integer to print
- */
-void print128(__int128 x)
-{
-	if (x == 0)
-		return;
-	print128(x / 10);
-	putchar('0' + (int)(x % 10));
-}
-/**
  * factorize - Factorize an integer into two smaller integers
  * @n: The integer to factorize
  */
-void factorize(__int128 n)
+void factorize(long long n)
 {
-	__int128 i;
+	long long i;
 
 	for (i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
 		{
-			print128(n);
-			putchar('=');
-			print128(n / i);
-			putchar('*');
-			print128(i);
-			putchar('\n');
+			printf("%lld=%lld*%lld\n", n, n / i, i);
 			return;
 		}
 	}
@@ -61,7 +28,7 @@ int main(int argc, char *argv[])
 {
 	char *filename, line[256];
 	FILE *file;
-	__int128 n;
+	long long n;
 
 	if (argc != 2)
 		return (1);
@@ -73,7 +40,7 @@ int main(int argc, char *argv[])
 		return (1);
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
-		n = atoll128(line);
+		n = atoll(line);
 		factorize(n);
 	}
 	fclose(file);
